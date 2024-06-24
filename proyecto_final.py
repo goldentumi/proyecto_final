08cc96afaa9754a1750c56a4ede15531b3f40a6f
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-06-24 17:20:18 -0500

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -13,3 +13,13 @@ quechua = pd.read_excel('conjugaciones_quechua.xlsx')
 
 st.title(':rainbow[Conjugador inverso]')
 
+df_conjugaciones = pd.ExcelFile('conjugaciones_quechua.xlsx')
+
+# Dividir las columnas en listas
+columnas = df_conjugaciones.columns
+listas = {columna: df_conjugaciones[columna].tolist() for columna in columnas}
+
+# Imprimir las listas para verificar
+for columna, lista in listas.items():
+    print(f"Columna: {columna}")
+    print(lista)
