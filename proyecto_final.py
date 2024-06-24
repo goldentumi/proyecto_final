30ea9607b330a2033f8c6b1d71aa931e02b1a937
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-06-24 17:05:02 -0500

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -5,3 +5,11 @@ Created on Mon Jun 24 16:58:13 2024
 @author: gaby-
 """
 
+import pandas as pd
+import streamlit as st
+
+archivo = open('conjugaciones_quechua.xlsx')
+quechua = pd.read_excel('conjugaciones_quechua.xlsx')
+
+st.title(':rainbow[Conjugador inverso]')
+
