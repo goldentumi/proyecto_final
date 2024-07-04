904be000d620f3f23c5ab6d0239bd2df3fa9117e
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-07-03 22:47:38 -0500

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -102,11 +102,12 @@ st.title(':rainbow[**Conjugador inverso**]', anchor = False)
 
 #¿Qué hace un conjugador inverso?
 #st.button('¿Qué es este conjugador?')
-st.button(':leftwards_arrow_with_hook:')
-if st.button('¿Qué es este conjugador?'):
-    st.write('Este :blue[**conjugador inverso**] descompone la conjugación seleccionada para que puedas conocer sus componentes :smile:')
+
+st.button('¿Qué es este conjugador?'):
+if st.button(':leftwards_arrow_with_hook:')
+   st.write('')
 else:
-    ''
+    st.write('Este :blue[**conjugador inverso**] descompone la conjugación seleccionada para que puedas conocer sus componentes :smile:')
 # Selección de verbo en español
 verbo_espanol = st.selectbox(":violet-background[Seleccione un verbo en español:]", list(verbos['español']))
 verbo_quechua = dict(zip(verbos['español'], verbos['quechua']))[verbo_espanol]
