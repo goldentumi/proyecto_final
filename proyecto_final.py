817480c254caff798c2027e82571024da5c9750c
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-07-03 22:07:33 -0500

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -83,31 +83,31 @@ st.write("El verbo en español es:", dict(zip(verbos['quechua'], verbos['españo
 if base.endswith("y"):
     base = base[:-1]
 st.subheader('**Persona**', divider='rainbow')
-persona = st.selectbox("Seleccione una persona:", ["primera inclusiva", "primera exclusiva", "segunda", "tercera"])
+persona = st.selectbox(":violet-background[Seleccione una persona:]", ["primera inclusiva", "primera exclusiva", "segunda", "tercera"])
 st.subheader('**Número**', divider='rainbow')
-numero = st.selectbox("Seleccione un número:", ["singular", "plural"])
+numero = st.selectbox(":violet-background[Seleccione un número:]", ["singular", "plural"])
 st.subheader('**Tiempo**', divider='rainbow')
-tiempo = st.selectbox("Seleccione un tiempo:", ["presentesimple","presenteprogresivo", "presentehabitual", "pasadoexperimentadosimple", "pasadoexperimentadoprogresivo", "pasadoexperimentadohabitual", "pasadonoexperimentadosimple", "pasadonoexperimentadoprogresivo", "pasadonoexperimentadohabitual"])
+tiempo = st.selectbox(":violet-background[Seleccione un tiempo:]", ["presentesimple","presenteprogresivo", "presentehabitual", "pasadoexperimentadosimple", "pasadoexperimentadoprogresivo", "pasadoexperimentadohabitual", "pasadonoexperimentadosimple", "pasadonoexperimentadoprogresivo", "pasadonoexperimentadohabitual"])
 d_tiempo = {'presentesimple':'El **presente simple** es equivalente a las formas castellanas *yo como; tú bailas,* etc. En quechua, el presente simple es el tiempo no marcado, es decir que no hay una marca explícita que indique tal tiempo; por el contrario, para conjugar un verbo en presente simple solo es necesario añadir las marcas personales a la raíz verbal.','presenteprogresivo':'Llamamos **presente progresivo** a la forma del presente equivalente a las perífrasis verbales castellanas *estoy comiendo* o *estoy bailando*. Es decir que el presente progresivo se emplea cuando el **evento descrito por una oración está ocurriendo mientras dicha oración es pronunciada**. En otras palabras, el presente progresivo se emplea cuando el evento referido por la oración es simultáneo al acto de habla. Para conjugar verbos en presente progresivo solo es necesario anteponer el sufijo **-chka** a las marcas personales del presente simple.','presentehabitual': 'Llamamos **presente habitual** a la forma del presente equivalente a las perífrasis verbales castellanas *suelo caminar* o *suelo hablar*. Es decir que el presente habitual se emplea cuando el **evento descrito por una oración es cotidiano, natural y se da con cierta regularidad**. Para conjugar verbos en presente habitual es necesario hacer uso de una perífrasis verbal, en la que añadiremos el sufijo **–q** al verbo principal y emplearemos el verbo ser, **kay**, conjugado en presente simple, a manera de auxiliar. Como puedes apreciar, en el caso de la :blue[**tercera persona no se debe incluir el verbo ser, sino que es suficiente con el sufijo –mi**], tal como ocurría con el presente simple.','pasadoexperimentadosimple': 'Esta forma de pasado se emplea cuando narramos hechos de los cuales hemos sido :blue[**testigos directos**]; es decir, hechos que nos constan. Por este valor, las distintas gramáticas quechuas llaman a este tiempo **pasado experimentado**, ya que lo que se está indicando es que el hablante ha experimentado lo que está diciendo. Se conjuga agregando el sufijo **-rqa** antes de la marca de persona.','pasadoexperimentadoprogresivo' : 'Esta forma de pasado se emplea cuando narramos hechos de los cuales hemos sido :blue[**testigos directos**]; es decir, hechos que nos constan. Por este valor, las distintas gramáticas quechuas llaman a este tiempo pasado experimentado, ya que lo que se está indicando es que el hablante ha experimentado lo que está diciendo. Se conjuga agregando el sufijo **-rqa** después de la marca de progresivo y antes de la marca de persona.','pasadoexperimentadohabitual' : 'Esta forma de pasado se emplea cuando narramos hechos de los cuales hemos sido :blue[**testigos directos**]; es decir, hechos que nos constan. Por este valor, las distintas gramáticas quechuas llaman a este tiempo pasado experimentado, ya que lo que se está indicando es que el hablante ha experimentado lo que está diciendo. Se conjuga agregando el sufijo **-rqa** en el verbo **kay**, antes de la marca de persona.','pasadonoexperimentadosimple': 'El sufijo **–sqa** se emplea cuando se quiere hablar de hechos de los cuales :blue[**no hemos sido testigos directos**]; es decir hechos sobre los cuales no estamos seguros porque no nos constan. Este tiempo se usa, por ejemplo, para **contar mitos, cuentos, chistes y leyendas**.','pasadonoexperimentadoprogresivo' :	'El sufijo **–sqa** se emplea cuando se quiere hablar de hechos de los cuales :blue[**no hemos sido testigos directos**]; es decir hechos sobre los cuales no estamos seguros porque no nos constan. Este tiempo se usa, por ejemplo, para **contar mitos, cuentos, chistes y leyendas**. Se conjuga agregando dicho sufijo después de la marca de progresivo y antes de la marca de persona.','pasadonoexperimentadohabitual':	'El sufijo **–sqa** se emplea cuando se quiere hablar de hechos de los cuales :blue[**no hemos sido testigos directos**]; es decir hechos sobre los cuales no estamos seguros porque no nos constan. Este tiempo se usa, por ejemplo, para **contar mitos, cuentos, chistes y leyendas**. Se conjuga agregando dicho sufijo al verbo **kay**, antes de la marca de persona.'}
 with st.popover (":violet-background[:violet[💭 Da click aquí para conocer más sobre los tiempos.]]"):
    st.markdown(d_tiempo[tiempo])
 
-st.write("Seleccionaste:", persona, numero, tiempo)
-st.write("El verbo conjugado es:", conjugador(base, persona, numero, tiempo))
+st.write(":rainbow[**Seleccionaste:**]", persona, numero, tiempo)
+st.write(":green[**El verbo conjugado es:**]", conjugador(base, persona, numero, tiempo))
 
 
 st.title(':rainbow[**Conjugador inverso**]')
 # Selección de verbo en español
-verbo_espanol = st.selectbox("Seleccione un verbo en español:", list(verbos['español']))
+verbo_espanol = st.selectbox(":violet-background[Seleccione un verbo en español:]", list(verbos['español']))
 verbo_quechua = dict(zip(verbos['español'], verbos['quechua']))[verbo_espanol]
 st.write("El verbo en quechua es:", verbo_quechua)
 
 # Selección de conjugación en quechua filtrada por el verbo en español
 conjugaciones_filtradas = df_conjugaciones[df_conjugaciones['Verbo base (español)'] == verbo_espanol]['Conjugación']
-conjugacion_quechua = st.selectbox("Seleccione una conjugación en quechua:", conjugaciones_filtradas)
+conjugacion_quechua = st.selectbox(":violet-background[Seleccione una conjugación en quechua:]", conjugaciones_filtradas)
 
 # Inverso: Elegir una conjugación
-conjugacion_quechua = st.selectbox("Seleccione una conjugación en quechua:", df_conjugaciones['Conjugación'])
+conjugacion_quechua = st.selectbox(":violet-background[Seleccione una conjugación en quechua:]", df_conjugaciones['Conjugación'])
 if conjugacion_quechua:
     base, persona, numero, tiempo = descomponer_conjugacion(conjugacion_quechua)
     if base and persona and numero and tiempo:
